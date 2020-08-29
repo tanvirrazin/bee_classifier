1c1f5a2fd0eb1ce38673a2ac5ffe0f44c011d13b
Tanvir <tanvirrazin@gmail.com>
2020-08-29 16:17:18 -0400

    Added data

---
+++ models/research/object_detection/generate_tfrecords.sh
@@ -0,0 +1,3 @@
+rm *.record
+python generate_tfrecord.py --csv_input=images/train_labels.csv --image_dir=images/train --output_path=train.record
+python generate_tfrecord.py --csv_input=images/test_labels.csv --image_dir=images/test --output_path=test.record
